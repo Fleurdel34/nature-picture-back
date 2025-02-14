2de29935a5be43755826c2e5fb2711b9c1fd4dbd
Fleurdel34 <betdevelweb0953@gmail.com>
2025-02-14 18:47:40 +0100

    modified models Form add property Date

--- src/main/java/com/naturepictureback/models/Form.java
+++ src/main/java/com/naturepictureback/models/Form.java
@@ -3,6 +3,8 @@ package com.naturepictureback.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.Date;
+
 
 @Data
 @Entity
@@ -21,12 +23,17 @@ public class Form {
     @Column(nullable = false)
     private String filename;
 
+    @Column(nullable = false)
+    private Date date;
+
     public Form() {
     }
 
-    public Form(String title, String description, String filename) {
+
+    public Form(String title, String description, String filename, Date date) {
         this.title = title;
         this.description = description;
         this.filename = filename;
+        this.date = date;
     }
 }
